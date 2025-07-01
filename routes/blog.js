3e97e8d6e93b8c33b6ef68785ac6d6aaefd1b1f2
persontyping <persontyping@pm.me>
2025-07-01 18:49:20 +0800

    change blog route

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET blog page. */
-router.get('/', function(req, res, next) {
-  res.render('blog', { title: 'Blog' });
+// This responds to /blog/
+router.get('/', (req, res) => {
+  res.send('Blog home page');
+});
+
+// Example additional route: /blog/post
+router.get('/post', (req, res) => {
+  res.send('Blog post');
 });
 
 module.exports = router;
