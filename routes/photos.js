e8dc4429decd5404ac019e844fb7fe9dc77c1a7e
persontyping <persontyping@pm.me>
2025-07-11 15:09:25 +0800

    changing card to be a mixin and blog posts on blog page to be a list

--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET photos page with demo cards */
 router.get('/', function(req, res, next) {
-  res.render('photos', { title: 'Photos' });
+  const demoCards = [
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=284836&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Click me' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=d3d60a&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Learn more' },
+    { title: 'Card 3', text: 'Demo text 3', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=a10c81&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Buy now' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=284836&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Click me' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=d3d60a&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Learn more' },
+    { title: 'Card 3', text: 'Demo text 3', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=a10c81&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Buy now' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=284836&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Click me' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=d3d60a&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Learn more' },
+    { title: 'Card 3', text: 'Demo text 3', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=a10c81&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Buy now' }
+  ];
+
+  res.render('photos', { title: 'Photos', demoCards });
 });
 
 module.exports = router;
