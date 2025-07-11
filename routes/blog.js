ac0ac8508b212ebbfd7e25e3f56a05d0aafa49b5
persontyping <persontyping@pm.me>
2025-07-11 16:03:57 +0800

    added styling and image to single post

--- routes/blog.js
+++ routes/blog.js
@@ -6,21 +6,23 @@ const posts = [
 
   {
     postId: '1',
-    title: 'Another Post',
-    content: 'Here is some _italic_ text and a [link](https://example.com).',
+    title: 'The Big Mistake',
+    imageUrl: 'https://imageslot.com/v1/1080x1080?bg=ff61ad&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png',
+    content: 'Here is some _italic_ text and a [link](https://persontyping.xyz).',
     date: '2024-06-25'
   },
     {
     postId: '2',
-    title: 'Another Post',
-    content: 'Here is some _italic_ text and a [link](https://example.com).',
+    title: 'All the Pizza in the World',
+    imageUrl: 'https://imageslot.com/v1/1080x1080?bg=fcff61&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png',
+    content: 'Here is some _italic_ text and a [link](https://persontyping.xyz).',
     date: '2024-06-25'
   }
 ];
 
 // Blog home page (list)
 router.get('/', (req, res) => {
-  res.render('blog', { title: 'Blog', posts: Object.values(posts) });
+  res.render('blog', { title: 'The 999 News', posts: Object.values(posts) });
 });
 
 // GET single blog post
@@ -28,11 +30,7 @@ router.get('/:postId', (req, res, next) => {
   const post = posts.find(p => p.postId === req.params.postId);
   if (!post) return next(); // 404 fallback
 
-  res.render('post', {
-    title: post.title,
-    pageTitle: post.title,
-    post
-  });
+  res.render('post', { post });
 });
 
 module.exports = router;
