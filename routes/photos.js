d4ce7bb7b11c2e1ebe4671410610e441d5d10796
persontyping <persontyping@pm.me>
2025-07-21 18:39:37 +0800

    photo page to camera icon default

--- routes/photos.js
+++ routes/photos.js
@@ -4,15 +4,13 @@ var router = express.Router();
 /* GET photos page with demo cards */
 router.get('/', function(req, res, next) {
   const demoCards = [
-    { title: 'Card 1', text: 'Demo text 1', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=284836&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Click me' },
-    { title: 'Card 2', text: 'Demo text 2', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=d3d60a&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Learn more' },
-    { title: 'Card 3', text: 'Demo text 3', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=a10c81&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Buy now' },
-    { title: 'Card 1', text: 'Demo text 1', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=284836&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Click me' },
-    { title: 'Card 2', text: 'Demo text 2', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=d3d60a&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Learn more' },
-    { title: 'Card 3', text: 'Demo text 3', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=a10c81&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Buy now' },
-    { title: 'Card 1', text: 'Demo text 1', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=284836&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Click me' },
-    { title: 'Card 2', text: 'Demo text 2', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=d3d60a&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Learn more' },
-    { title: 'Card 3', text: 'Demo text 3', imageUrl: 'https://imageslot.com/v1/1080x1080?bg=a10c81&fg=ffffff&shadow=23272f&text=%5BIMAGE%5D&filetype=png', buttonUrl: '#', buttonText: 'Buy now' }
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Click me' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Learn more' },
+    { title: 'Card 3', text: 'Demo text 3', imageUrl: '', buttonUrl: '#', buttonText: 'Buy now' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Buy now' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Click me' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Learn more' },
+    { title: 'Card 3', text: 'Demo text 3', imageUrl: '', buttonUrl: '#', buttonText: 'Buy now' }
   ];
 
   res.render('photos', { title: 'Photos', demoCards });
