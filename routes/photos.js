90447396b76d23099cc691d9fb82a0e8511fc4e3
persontyping <persontyping@pm.me>
2025-07-23 23:47:22 +0800

    styled the media cards

--- routes/photos.js
+++ routes/photos.js
@@ -4,16 +4,18 @@ var router = express.Router();
 /* GET photos page with demo cards */
 router.get('/', function(req, res, next) {
   const demoCards = [
-    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Click me' },
-    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Learn more' },
-    { title: 'Card 3', text: 'Demo text 3', imageUrl: '', buttonUrl: '#', buttonText: 'Buy now' },
-    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Buy now' },
-    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Click me' },
-    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Learn more' },
-    { title: 'Card 3', text: 'Demo text 3', imageUrl: '', buttonUrl: '#', buttonText: 'Buy now' }
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 1', text: 'Demo text 1', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' },
+    { title: 'Card 2', text: 'Demo text 2', imageUrl: '', buttonUrl: '#', buttonText: 'Read More' }
+
   ];
 
-  res.render('photos', { title: 'Photos', demoCards });
+  res.render('photos', { title: 'Media', demoCards });
 });
 
 module.exports = router;
