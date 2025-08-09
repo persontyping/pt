b09cbf77809daba77957f481d306d198e8e38e96
persontyping <persontyping@pm.me>
2025-08-10 01:25:53 +0800

    messing w/ title size

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "papercliprepublic.org" });
+  res.render('index', { title: "paperclip republic . org 📎" });
 });
 
 module.exports = router;
